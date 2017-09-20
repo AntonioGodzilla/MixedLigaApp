d33eceec167b6d8de784495a092f19a2b6bbcdc1
AntonioGodzilla <32140014+AntonioGodzilla@users.noreply.github.com>
2017-09-20 21:51:17 +0200

    Main

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using System.Globalization;
+using System.Windows;
+using WpfApplicationLiga.ViewModels;
+
+namespace WpfApplicationLiga
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //Properties.Resources.Culture = new CultureInfo(ConfigurationManager.AppSettings["Culture"]);
+            //System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo("de-DE");
+            //System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
+
+            InitializeComponent();
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //for (int i = 0; i < 10000000; i++)
+            //{
+            //    txtVersion.Text = i.ToString();
+
+            //}
+            
+
+            LigaAnalyseViewModel myStartWindow = new LigaAnalyseViewModel();
+            //WindowControlTesterViewModel myStartTest = new WindowControlTesterViewModel();
+            //myStartTest.Initialize();
+
+            myStartWindow.Initialize();
+            this.Visibility = System.Windows.Visibility.Hidden;
+            this.Close();
+        }
+    }
+}
