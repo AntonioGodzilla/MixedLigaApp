a0a4ec4db8ea5b90b66f46aedffa786dd800edb8
AntonioGodzilla <32140014+AntonioGodzilla@users.noreply.github.com>
2017-09-20 19:44:24 +0200

    Views

---
+++ Views/Controls/BegegnungenEditierenView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Configuration;
+
+namespace WpfApplicationLiga.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for BegegnungenEditierenView.xaml
+    /// </summary>
+    public partial class BegegnungenEditierenView : UserControl
+    {
+        public BegegnungenEditierenView()
+        {
+            //Properties.Resources.Culture = new CultureInfo(ConfigurationManager.AppSettings["Culture"]);
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/Windows/LigaAnalyseView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Configuration;
+
+namespace WpfApplicationLiga.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for LigaAnalyseView.xaml
+    /// </summary>
+    public partial class LigaAnalyseView : Window
+    {
+        public LigaAnalyseView()
+        {
+            //Properties.Resources.Culture = new CultureInfo(ConfigurationManager.AppSettings["Culture"]);
+            InitializeComponent();
+        }
+
+        private void TabControlTabelle_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            MessageBox.Show("Event");
+        }
+
+    }
+}
